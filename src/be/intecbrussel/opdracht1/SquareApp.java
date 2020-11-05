9e4eb7da929090fc8e94aba4633d171d6468c09a
Audric <onockx.a@gmail.com>
2020-11-05 15:02:20 +0100

    counts

--- src/be/intecbrussel/opdracht1/SquareApp.java
+++ src/be/intecbrussel/opdracht1/SquareApp.java
@@ -3,6 +3,6 @@ package be.intecbrussel.opdracht1;
 public class SquareApp {
     public static void main(String[] args) {
         Rectangle square = new Square(2);
-        square = new Rectangle();
+
     }
 }
